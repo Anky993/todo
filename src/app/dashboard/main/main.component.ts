a0bec374cb48d06041aeb03f69ecb8085535d461
Anky993 <chauhan.arcveer@gmail.com>
2021-02-02 23:27:49 +0530

    changes some functionlities

--- src/app/dashboard/main/main.component.ts
+++ src/app/dashboard/main/main.component.ts
@@ -14,43 +14,64 @@ export class MainComponent implements OnInit {
 
   constructor(private renderer: Renderer2) { }
   @ViewChild('titleDisplay') menu: ElementRef;
+  @ViewChild('takeANote') takeANoteFocus: ElementRef;
 
   ngOnInit(): void {
   }
 
   takeNote() {
-    setTimeout(() => {
-      const elem = this.renderer.selectRootElement('#takeANote');
-      // console.log("focus onssssdsds jsj ",this.renderer.selectRootElement('#titleDisplay') )
-
-      this.renderer.listen(elem, 'focus', () => {
-
-        this.renderer.listen('window', 'click', (e: Event) => {
-         if ( e.target !== this.menu.nativeElement  ) {
-            console.log('clicked outside');
-        }
-     });
-        console.log('focus on ');
-        // var element = this.renderer.selectRootElement('#takeANote');
-        // this.renderer.listen(elem, "focus", () => {
-        // console.log("focus ossssn ")
-
-        // })
-          // element.focus();
-       });
-
-      // this.renderer.listen(elem, "blur", () => {
-      //   console.log("blur")
-      //   this.takeANoteDisplay = {display : 'block' }
-      //   this.tilteDisplay = {display : 'none'}
-      //  });
-      elem.focus();
-    }, 0);
-
-    console.log('sds');
-    this.takeANoteDisplay = {display : 'none' };
-    this.tilteDisplay = {display : 'block'};
+    this.takeANoteDisplay =  {
+      display : 'none'
+    };
+    this.tilteDisplay = {
+      display : 'block'
+    };
 
   }
+
+  // takeNote() {
+  //   setTimeout(() => {
+  //     const elem = this.renderer.selectRootElement('#takeANote');
+  //     // console.log("focus onssssdsds jsj ",this.renderer.selectRootElement('#titleDisplay') )
+  //     let b = false
+  //     this.renderer.listen(elem, 'focus', () => {
+  //       let f = true
+
+  //       this.renderer.listen('window', 'click', (e: Event) => {
+  //         let g = document.getElementById('titleDisplay')
+
+
+  //         console.log("foce::;;, ,,", document.activeElement == g )
+  //        if ((document.activeElement !== g ) &&  e.target !== this.menu.nativeElement ) {
+  //           console.log('clicked outside',e);
+  //       }
+  //    });
+  //    this.renderer.listen(elem, "blur", () => {
+  //     //   console.log("blur")
+  //     //   this.takeANoteDisplay = {display : 'block' }
+  //     //   this.tilteDisplay = {display : 'none'}
+  //      })
+  //       console.log('focus on ');
+  //       // var element = this.renderer.selectRootElement('#takeANote');
+  //       // this.renderer.listen(elem, "focus", () => {
+  //       // console.log("focus ossssn ")
+
+  //       // })
+  //         // element.focus();
+  //      });
+
+  //     // this.renderer.listen(elem, "blur", () => {
+  //     //   console.log("blur")
+  //     //   this.takeANoteDisplay = {display : 'block' }
+  //     //   this.tilteDisplay = {display : 'none'}
+  //     //  });
+  //     elem.focus();
+  //   }, 0);
+
+  //   console.log('sds');
+  //   this.takeANoteDisplay = {display : 'none' };
+  //   this.tilteDisplay = {display : 'block'};
+
+  // }
 
 }
